81a7b631f03143fec5bce2e84266c80d1086318e
dwivAnsh <anshuldwivedi012@gmail.com>
2025-05-18 15:55:44 +0530

    font updated montserrat

--- src/App.jsx
+++ src/App.jsx
@@ -237,7 +237,7 @@ const App = () => {
                       Download Now
                     </button>
                   </div>
-                </div>
+                </div>  
               </div>
             </div>
         </ScrollProvider>
