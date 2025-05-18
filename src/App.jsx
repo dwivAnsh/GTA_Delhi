d56e0dd2c683f46b3a47025a6adbe9c0c62d039c
dwivAnsh <anshuldwivedi012@gmail.com>
2025-05-18 16:30:50 +0530

    Changed Open Graph tags

--- src/App.jsx
+++ src/App.jsx
@@ -154,92 +154,93 @@ const App = () => {
 
       {/* Main Content */}
       {showContent && (
-        <ScrollProvider>  
-            <div className="main w-full rotate-[10deg] scale-[1.7]">
-              <div className="landing w-full h-screen bg-black relative overflow-hidden">
-                <div className="navbar absolute top-5 left-5 w-[94%] z-[11]">
-                  <div className="logo flex gap-5 items-center">
-                    <div className="lines flex flex-col gap-1">
-                      <div className="line w-12 h-1.5 bg-white"></div>
-                      <div className="line w-7 h-1.5 bg-white"></div>
-                      <div className="line w-5 h-1.5 bg-white"></div>
-                    </div>
-                    <div className="text-[2.3vw] text-white -translate-y-1">
-                      Rockstar
-                    </div>
+        <ScrollProvider>
+          <div className="main w-full rotate-[10deg] scale-[1.7]">
+            <div className="landing w-full h-screen bg-black relative overflow-hidden">
+              <div className="navbar absolute top-5 left-5 w-[94%] z-[11]">
+                <div className="logo flex gap-5 items-center">
+                  <div className="lines flex flex-col gap-1">
+                    <div className="line w-12 h-1.5 bg-white"></div>
+                    <div className="line w-7 h-1.5 bg-white"></div>
+                    <div className="line w-5 h-1.5 bg-white"></div>
                   </div>
-                </div>
-                <div className="imagesdiv relative w-full h-screen">
-                  <img
-                    className="sky absolute scale-[1.7] rotate-[20deg] top-0 left-0 w-full h-full object-cover"
-                    src="./sky.png"
-                    alt=""
-                  />
-                  <img
-                    className="bg scale-[2.1] rotate-[5deg] absolute top-0 left-0 w-full h-full object-cover"
-                    src="./bg.png"
-                    alt=""
-                  />
-                  <div
-                    className="text text-white w-full flex flex-col gap-[2vw] scale-[1.7] rotate-[-10deg]"
-                    style={{ textShadow: "10px 4px 10px rgba(0, 0, 0, 1)" }}
-                  >
-                    <h1 className="text-9xl absolute left-[42%] -translate-x-1/2">
-                      grand
-                    </h1>
-                    <h1 className="text-9xl absolute left-[68%] top-30 -translate-x-1/2">
-                      theft
-                    </h1>
-                    <h1 className="text-9xl absolute left-[35%] top-60 -translate-x-1/2">
-                      auto
-                    </h1>
+                  <div className="text-[2.3vw] text-white -translate-y-1">
+                    Rockstar
                   </div>
-                  <img
-                    className="character absolute -bottom-[100%] left-1/2 scale-[3] rotate-[-7deg] -translate-x-1/2"
-                    src="./girlbg.png"
-                    alt=""
-                  />
-                </div>
-                <div className="btmbar absolute bottom-0 flex items-center left-0 w-full h-[16%] z-[11] bg-gradient-to-t from-black to-transparent ">
-                  <div className="dwn flex gap-2 absolute items-center text-white bottom-2">
-                    <i className="text-2xl ri-arrow-down-line"></i>
-                    <h3 className="font-[Montserrat] text-xl">Scroll Down</h3>
-                  </div>
-                  <img
-                    className="absolute h-[3vw] left-1/2 bottom-1/2 -translate-x-1/2 translate-y-6/5"
-                    src="./ps5.png"
-                    alt=""
-                  />
                 </div>
               </div>
-              <div className="second relative w-full h-screen bg-black px-10 flex items-center justify-center overflow-hidden">
-                <div className="cntnr w-full h-[80%] flex text-white">
-                  <div className="limg w-1/2 h-full relative">
-                    <img
-                      className="absolute scale-[0.7] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
-                      src="./imag.png"
-                      alt=""
-                    />
-                  </div>
-                  <div className="rimg absolute left-3/3 top-2/7 -translate-x-1/2 -translate-y-1/2 w-full">
-                    <h1 className="text-8xl">Still Running,</h1>
-                    <h1 className="text-8xl">Not Hunting</h1>
-                    <p className="absolute top-60 font-[Montserrat] w-[40%]">
-                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                      Rerum alias nam ipsam. Sed, id. Soluta saepe autem placeat
-                      cumque possimus, ullam ad ducimus.
-                    </p>
-                    <p className="absolute top-85 font-[Montserrat] w-[40%]">
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                      Minima alias id non quae!
-                    </p>
-                    <button className="bg-yellow-300 text-black h-12 w-[12vw] text-2xl rounded-xs absolute top-110 hover:shadow-[0_0_15px_#facc15] transition duration-300">
-                      Download Now
-                    </button>
-                  </div>
-                </div>  
+              <div className="imagesdiv relative w-full h-screen">
+                <img
+                  className="sky absolute scale-[1.7] rotate-[20deg] top-0 left-0 w-full h-full object-cover"
+                  src="./sky.png"
+                  alt=""
+                />
+                <img
+                  className="bg scale-[2.1] rotate-[5deg] absolute top-0 left-0 w-full h-full object-cover"
+                  src="./bg.png"
+                  alt=""
+                />
+                <div
+                  className="text text-white w-full flex flex-col gap-[2vw] scale-[1.7] rotate-[-10deg]"
+                  style={{ textShadow: "10px 4px 10px rgba(0, 0, 0, 1)" }}
+                >
+                  <h1 className="text-9xl absolute left-[42%] -translate-x-1/2">
+                    grand
+                  </h1>
+                  <h1 className="text-9xl absolute left-[68%] top-30 -translate-x-1/2">
+                    theft
+                  </h1>
+                  <h1 className="text-9xl absolute left-[35%] top-60 -translate-x-1/2">
+                    auto
+                  </h1>
+                </div>
+                <img
+                  className="character absolute -bottom-[100%] left-1/2 scale-[3] rotate-[-7deg] -translate-x-1/2"
+                  src="./girlbg.png"
+                  alt=""
+                />
+              </div>
+              <div className="btmbar absolute bottom-0 flex items-center left-0 w-full h-[16%] z-[11] bg-gradient-to-t from-black to-transparent ">
+                <div className="dwn flex gap-2 absolute items-center text-white bottom-2">
+                  <i className="text-2xl ri-arrow-down-line"></i>
+                  <h3 className="font-[Montserrat] text-xl">Scroll Down</h3>
+                </div>
+                <img
+                  className="absolute h-[3vw] left-1/2 bottom-1/2 -translate-x-1/2 translate-y-6/5"
+                  src="./ps5.png"
+                  alt=""
+                />
               </div>
             </div>
+            <div className="second relative w-full h-screen bg-black px-10 flex items-center justify-center overflow-hidden">
+              <div className="cntnr w-full h-[80%] flex text-white">
+                <div className="limg w-1/2 h-full relative">
+                  <img
+                    className="absolute scale-[0.7] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+                    src="./imag.png"
+                    alt=""
+                  />
+                </div>
+                <div className="rimg absolute left-3/3 top-2/7 -translate-x-1/2 -translate-y-1/2 w-full">
+                  <h1 className="text-8xl">Still Running,</h1>
+                  <h1 className="text-8xl">Not Hunting</h1>
+                  <p className="absolute top-60 font-[Montserrat] w-[40%]">
+                    Welcome to <strong>GTA Delhi</strong> – the fusion of
+                    Delhi's chaos and Los Santos' thrill. Dive into India's most
+                    immersive GTA roleplay experience!
+                  </p>
+                  <p className="absolute top-85 font-[Montserrat] w-[40%]">
+                    Create your story, build your gang, rule the streets – only
+                    at GTA Delhi.
+                  </p>
+
+                  <button className="bg-yellow-300 text-black h-12 w-[12vw] text-2xl rounded-xs absolute top-110 hover:shadow-[0_0_15px_#facc15] transition duration-300">
+                    Download Now
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </ScrollProvider>
       )}
     </>
